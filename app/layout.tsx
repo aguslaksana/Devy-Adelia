0c1d0bc695c456a9a3235f360f9ca60c65f8793b
aguslaksana <aguslaksanasuputra@gmail.com>
2025-12-24 22:41:33 +0800

    membersihkan file layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,29 +1,21 @@
-"use client";
-
+import type { Metadata } from "next";
 import "./globals.css";
 
-import { MusicProvider, MusicButton } from "./music-context";
-import OrientationChecker from "./OrientationChecker";
+export const metadata: Metadata = {
+  title: "Game Deskripfun",
+  description: "Game Edukasi Kebudayaan",
+};
 
 export default function RootLayout({
-	children,
+  children,
 }: Readonly<{
-	children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-	return (
-		<html className="bg-white font-sans leading-normal tracking-normal">
-<<<<<<< HEAD
-			<title>Game Deskripfun</title>
-=======
-			<title>Game deskripfun</title>
->>>>>>> a5b0ac5e68094f2a11141b378d5385f1f7c3ea5c
-			<body className="flex flex-col min-h-screen">
-				<OrientationChecker />
-				<MusicProvider>
-					<main className="flex-grow">{children}</main>
-					<MusicButton />
-				</MusicProvider>
-			</body>
-		</html>
-	);
+  return (
+    <html lang="id">
+      <body className="bg-white font-sans leading-normal tracking-normal">
+        {children}
+      </body>
+    </html>
+  );
 }
